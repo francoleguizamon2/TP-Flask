c83cc2b48b311358a81d6be940c553f5153c871a
francoleguizamon2 <ftleguizamon@mail.austral.edu.ar>
2025-05-26 10:34:56 -0300

    arreglé unas cosas

--- application/models.py
+++ application/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from . import db
 
 class StudyAbroad(db.Model):
@@ -7,3 +8,12 @@ class StudyAbroad(db.Model):
     tuition = db.Column(db.Float)
     living_cost = db.Column(db.Float)
     total = db.Column(db.Float)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc) )
+
+    def __repr__(self):
+        return 'Task %r' % self.id
---
+++ main.py
@@ -0,0 +1,56 @@
+from application import create_app
+
+app = create_app()
+
+
+# with app.app_context():
+#     db.create_all()
+
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
+#     if request.method == 'POST':
+#         task_content = request.form['content'] 
+#         new_task = Todo(content=task_content)
+
+#         try:
+#             db.session.add(new_task)
+#             db.session.commit()
+#             return redirect('/')
+#         except: 
+#             return "There was an error adding your taks"
+
+#     else:
+#         tasks = Todo.query.order_by(Todo.date_created).all()
+#         return render_template('index.html', tasks=tasks)
+
+
+# @app.route('/delete/<int:id>')
+# def delete(id):
+#     task_to_delete = Todo.query.get_or_404(id)
+
+#     try:
+#         db.session.delete(task_to_delete)
+#         db.session.commit()
+#         return redirect('/')
+#     except:
+#         return "There was an error deleting your task"
+    
+
+# @app.route('/update/<int:id>', methods=['POST', 'GET'])
+# def update(id):
+#     task_to_update = Todo.query.get_or_404(id)
+
+#     if request.method == 'POST':
+#         task_to_update.content = request.form['content']
+#         try:
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return "There was an error updating your taks"
+#     else:
+#         return render_template('update.html', task=task_to_update)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
