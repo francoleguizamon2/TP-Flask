e1741f3972cbf0b9130b46207abfaacae0608804
Franco <ftleguizamon@mail.austral.edu.ar>
2025-07-11 02:05:11 -0300

    Paises no es mas obligatorio y la pagina es mas atractiva

--- application/models.py
+++ application/models.py
@@ -16,11 +16,5 @@ class StudyAbroad(db.Model):
     insurance = db.Column(db.Float)
     exchange_rate = db.Column(db.Float)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc) )
-
     def __repr__(self):
         return 'Task %r' % self.id
